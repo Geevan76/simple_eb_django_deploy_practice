d77de8a6d47111f73cca47cf5d897701195aaf4e
Geevan76 <cardtrend76@gmail.com>
2025-04-14 17:42:56 +0800

    complete working app without styling

--- project_1/settings.py
+++ project_1/settings.py
@@ -168,14 +168,14 @@ AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 
-# STORAGES = {
-#     "default": {            
-#         "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
-#     },
-#     "staticfiles": {
-#         "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
-#     },
-# }
+STORAGES = {
+    "default": {            
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+    },
+    "staticfiles": {
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+    },
+}
 
 AWS_S3_FILE_OVERWRITE = False
 
